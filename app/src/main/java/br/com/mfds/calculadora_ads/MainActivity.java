aa0b113e4d915398506e52d905aa1567fce31754
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2022-07-05 19:40:42 -0300

    feat: Inicialização do Menu e do fragment da Calculadora

--- app/src/main/java/br/com/mfds/calculadora_ads/MainActivity.java
+++ app/src/main/java/br/com/mfds/calculadora_ads/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import br.com.mfds.calculadora_ads.calculadora.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_main, new MainFragment()).commit();
+        }
     }
 }
